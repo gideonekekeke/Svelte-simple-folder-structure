f47dac3bd56639f1d87c7209d9b3247de0495e9b
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro.local>
2025-01-09 12:04:07 +0100

    added a mainlayout and a dashboard layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
 	import MainLayout from "./layouts/MainLayout.svelte";
 	import Home from "./pages/Home.svelte";
+  import About from "./pages/About.svelte";
+  import Contact from "./pages/Contact.svelte";
+	import DashboardLayout from "./layouts/DashboardLayout.svelte";
 
-  
+  const currentPath = window.location.pathname
+  const user = true
   
   
 </script>
@@ -10,29 +14,27 @@
 <main>
  
  
+ {#if !user} 
  <MainLayout>
    <div>
      <!-- Your Svelte app goes here -->
-      <Home  />
+    {#if currentPath === '/'}
+    <Home  />
+      {:else if currentPath === '/about'}
+      <About  />
+        {:else if currentPath === '/contact'}
+        <Contact  />
+         {:else}
+    <h1>404 - Page Not Found</h1>
+  {/if}
    </div>
  </MainLayout>
  
- 
- 
- 
- 
- 
-
- 
- 
- 
-
- 
- 
- 
-
- 
- 
+ {:else}
+ <DashboardLayout >
+  <div>this is the dashboard</div>
+ </DashboardLayout>
+ {/if}
  
 </main>
 
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,21 @@
 <script>
 
+    const links = [
+        {
+            label: 'Home',
+            url: '/contact'
+        },
+        {
+            label: 'About',
+            url: '/about'
+        }
+    ]
+
 </script>
 
 <nav>
     <div>Logo</div>
-    <div>Home</div>
-    <div>About</div>
+    <div><a href="/">Home</a></div>
+    <div><a href="/about">About</a></div>
+    <div><a href="/contact">Contact</a></div>
 </nav>
